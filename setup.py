f82697050bb6f476efc07fad8979c3e9676e6ab8
nvd <navidsoleymani@ymail.com>
2022-09-04 14:53:56 +0430

    conf

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     # author_email='email@example.com',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    url='https://github.com/beensi-com/responses',
+    url='https://github.com/beensi-packages/responses.git',
     # keywords='',
     install_requires=[
         'messages @ git+https://github.com/beensi-packages/messages.git@cb3f31b7c189aed1d8fd7cb53e5266b97df81bd4',
