6251b59401a733fd6d947cef21956c418953893c
nvd <navidsoleymani@ymail.com>
2022-09-04 21:30:49 +0430

    refactoring

--- src/responses/decorator/__init__.py
+++ src/responses/decorator/__init__.py
@@ -7,10 +7,16 @@ def clean_response(func):
     async def wrapper(*args, **kwargs):
         rsp = {}
         try:
+            if 'logger' in kwargs and 'request_url' in kwargs:
+                kwargs['logger'].info(f"Start requesting \'{kwargs['request_url']}\'.")
             rsp = await func(*args, **kwargs)
         except Exception as exp:
+            if 'logger' in kwargs:
+                kwargs['logger'].error(exp.args[0])
             rsp = exception(exp.args[0])
         finally:
+            if 'logger' in kwargs and 'request_url' in kwargs:
+                kwargs['logger'].info(f"End of request \'{kwargs['request_url']}\'.")
             return rsp
 
     return wrapper
